cedabcca8301b1fc2ce98a5ed8658e9d8e8c01f2
Raltour <15610159165lmz@gmail.com>
2025-12-01 18:23:03 +0800

    chore(main): 去掉了main中的标准输出，以后只用日志系统做打印

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,7 @@
 #include "TinyRPC/common/console_logger.h"
 #include "TinyRPC/common/config.h"
-#include <iostream>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
   LOG_INFO("Start Server");
 
   Config::GetInstance();
